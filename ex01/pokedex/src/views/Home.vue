ca690d378e24cfb49d333a9a79cab1f9fcafbdf7
Taisei Toya <tytaisei0102@gmail.com>
2021-01-14 06:17:48 +0100

    Add:
    Vue-cliで環境を構築

---
+++ ex01/pokedex/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <poke-list></poke-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import PokeList from '@/components/pokelist.vue';
+
+@Component({
+  components: {
+    PokeList,
+  },
+})
+export default class Home extends Vue {}
+</script>
