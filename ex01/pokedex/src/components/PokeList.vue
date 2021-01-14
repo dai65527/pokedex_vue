30e6613e97e78055ae97a304c61079ffe7269007
dnakano <dnakano@student.42tokyo.jp>
2021-01-14 22:33:56 +0900

    Ex01: Update: get pokemons as array from function

--- ex01/pokedex/src/components/PokeList.vue
+++ ex01/pokedex/src/components/PokeList.vue
@@ -1,14 +1,41 @@
 <template>
   <div>
     <h2>Pokelist</h2>
-    <PokeListItem :pokemon="pokemon" />
+    <PokeListItem
+      v-for="pokemon in pokemons"
+      :key="pokemon.id"
+      :pokemon="pokemon"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Prop, Component, Vue } from "vue-property-decorator";
+import axios from "axios";
 import PokeListItem from "./PokeListItem.vue";
 import Pokemon from "../models/Pokemon";
+
+const fetchPokemons = (): Pokemon[] => {
+  const p1 = new Pokemon(
+    1,
+    "Fushigidane",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+  );
+
+  const p2 = new Pokemon(
+    2,
+    "Fushigisou",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
+  );
+
+  const p3 = new Pokemon(
+    3,
+    "Fushigibana",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"
+  );
+
+  return [p1, p2, p3];
+};
 
 @Component({
   components: {
@@ -16,10 +43,11 @@ import Pokemon from "../models/Pokemon";
   },
 })
 export default class PokeList extends Vue {
-  private pokemon = new Pokemon(
-    1,
-    "Fushigidane",
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-  );
+  private pokemons: Pokemon[] = [];
+
+  // lifecycle hooks
+  mounted() {
+    this.pokemons = fetchPokemons();
+  }
 }
 </script>
