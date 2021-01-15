6576fcdaa362fbd78bf5e05c6493665b0f2335bd
dnakano <dnakano@student.42tokyo.jp>
2021-01-15 16:57:47 +0900

    Ex01: Update: add infinite scroll
    - npm vue-infinite-lodingした
    - 全てのデータが読み出せた
    - 謎に中国語が混じり出す

--- ex01/pokedex/src/components/PokeList.vue
+++ ex01/pokedex/src/components/PokeList.vue
@@ -6,25 +6,50 @@
       :key="pokemon.id"
       :pokemon="pokemon"
     />
+    <InfiniteLoading
+      v-if="!flgErrLoading || !flgErrLoading"
+      ref="infiniteLoading"
+      spinner="spiral"
+      @infinite="loadPokemons">
+      <span slot="no-more">以上</span>
+    </InfiniteLoading>
+    <!-- <button @click="loadPokemons">More</button> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import InfiniteLoading from "vue-infinite-loading";
 import PokeListItem from "./PokeListItem.vue";
 import Pokemon, { fetchPokemons } from "../models/Pokemon";
 
 @Component({
   components: {
-    PokeListItem
-  }
+    PokeListItem,
+    InfiniteLoading,
+  },
 })
 export default class PokeList extends Vue {
   private pokemons: Pokemon[] = [];
+  private numLoaded = 0;
+  private numToLoad = 20;
+  private flgErrLoading = false;
 
-  // lifecycle hooks
-  async mounted() {
-    this.pokemons = await fetchPokemons(0, 20);
+  async loadPokemons() {
+    const fetchedPokemons = await fetchPokemons(
+      this.numLoaded,
+      this.numToLoad
+    ).catch((error): Pokemon[] => {
+      console.error(error);
+      this.flgErrLoading = true;
+      return [];
+    });
+    this.numLoaded += fetchedPokemons.length;
+    this.pokemons = this.pokemons.concat(fetchedPokemons);
+    this.$refs.infiniteLoading.stateChanger.loaded();
+    if (fetchedPokemons.length < this.numToLoad) {
+      this.$refs.infiniteLoading.stateChanger.complete();
+    }
   }
 }
 </script>
