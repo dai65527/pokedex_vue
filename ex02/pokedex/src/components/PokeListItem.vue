fff081fc18e06b87578f0976ef81175b13f901c7
Taisei Toya <tytaisei0102@gmail.com>
2021-01-17 09:44:03 +0100

    Update: Add routerlink to v-card
    v-cardにルーターリンクを内包するよう変更
    v-cardにホバーを追加

--- ex02/pokedex/src/components/PokeListItem.vue
+++ ex02/pokedex/src/components/PokeListItem.vue
@@ -1,7 +1,10 @@
 <template>
   <v-col cols="12" xs="6" sm="4" md="3" xl="2">
-    <router-link :to="{ name: 'pokemon', params: { id: pokemon.id } }">
-      <v-card class="mx-auto">
+      <v-card 
+      hover
+      :ripple="false"
+      :to="{ name: 'pokemon', params: { id: pokemon.id } }"
+      class="mx-auto">
         <v-img :src="require('@/assets/pokeball_bg.png')" :alt="pokemon.name">
           <v-img :src="pokemon.imageUrl" :alt="pokemon.name"> </v-img>
         </v-img>
@@ -9,7 +12,6 @@
           "No." + pokemon.id + ": " + pokemon.name
         }}</v-card-title>
       </v-card>
-    </router-link>
   </v-col>
 </template>
 
