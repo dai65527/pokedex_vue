821aa0c189755c9b916afc995f20da36309c2ce1
Taisei Toya <tytaisei0102@gmail.com>
2021-01-17 10:54:25 +0100

    Update: logosize
    ロゴをbar内に収まるよう調整
    ロゴにpokedexへ遷移できるようリンクを設定

--- ex02/pokedex/src/components/NavBar.vue
+++ ex02/pokedex/src/components/NavBar.vue
@@ -1,7 +1,18 @@
 <template>
-  <v-app-bar app color="red" dark
-    ><div><v-img :src="require('@/assets/pokedex_logo.png')"></v-img></div
-  ></v-app-bar>
+  <div>
+    <v-app-bar app color="red">
+      <div class="mx-2">
+        <router-link :to="{ name: 'pokedex' }">
+          <v-img
+            :src="require('@/assets/pokedex_logo.png')"
+            max-height="56"
+            max-width="200"
+            contain
+          ></v-img>
+        </router-link>
+      </div>
+    </v-app-bar>
+  </div>
 </template>
 
 <script lang="ts">
