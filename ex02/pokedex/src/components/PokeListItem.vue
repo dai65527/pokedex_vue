49e3359df3141d99450928914621c066dfd908a6
dnakano <dnakano@student.42tokyo.jp>
2021-01-17 13:38:25 +0900

    Ex02: Update: PokeListItemコンポーネントにrouter-linkを追加
    - PokeListItemをクリックすると/pokemon/:idに遷移するようにした。

--- ex02/pokedex/src/components/PokeListItem.vue
+++ ex02/pokedex/src/components/PokeListItem.vue
@@ -1,13 +1,15 @@
 <template>
   <v-col cols="12" xs="6" sm="4" md="3" xl="2">
-    <v-card class="mx-auto">
-      <v-img :src="require('@/assets/pokeball_bg.png')" :alt="pokemon.name">
-        <v-img :src="pokemon.imageUrl" :alt="pokemon.name"> </v-img>
-      </v-img>
-      <v-card-title class="font-weight-black text-body-1">{{
-        "No." + pokemon.id + ": " + pokemon.name
-      }}</v-card-title>
-    </v-card>
+    <router-link :to="{ name: 'pokemon', params: { id: pokemon.id } }">
+      <v-card class="mx-auto">
+        <v-img :src="require('@/assets/pokeball_bg.png')" :alt="pokemon.name">
+          <v-img :src="pokemon.imageUrl" :alt="pokemon.name"> </v-img>
+        </v-img>
+        <v-card-title class="font-weight-black text-body-1">{{
+          "No." + pokemon.id + ": " + pokemon.name
+        }}</v-card-title>
+      </v-card>
+    </router-link>
   </v-col>
 </template>
 
