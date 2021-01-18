221c5125928438d54fd8d1f4821f0dc9ba11f162
Taisei Toya <tytaisei0102@gmail.com>
2021-01-18 12:58:13 +0100

    Update: Pokemon layout
    基本レイアウトを修正

--- ex02/pokedex/src/components/Pokemon.vue
+++ ex02/pokedex/src/components/Pokemon.vue
@@ -3,8 +3,8 @@
     <div class="bg" v-if="flgFetched">
       <v-container>
         <v-row justify="center" align-content="center">
-          <v-col cols="12" lg="6">
-            <v-row no-gutters justify="center" align-content="center">
+          <v-col cols="12" lg="6" class="my-auto">
+            <v-row justify="center" align-content="center" no-gutters>
               <v-col cols="10">
                 <v-img :src="pokeInfo.imageUrl" :alt="pokeInfo.name" />
               </v-col>
@@ -26,44 +26,42 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="my-10">
+              <v-col class="my-10 mx-auto" cols="10">
                 <v-card class="grey lighten-5" elevation="4">
-                  <v-row>
-                    <v-col>
-                      <v-card-text class="font-weight-bold display-1"
-                        >分類: たねポケモン</v-card-text
-                      >
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col>
-                      <v-card-text class="font-weight-bold display-1"
-                        >たかさ:
-                      </v-card-text>
-                    </v-col>
-                    <v-col>
-                      <v-card-text class="font-weight-bold display-1"
-                        >{{ pokeInfo.height }}m</v-card-text
-                      >
-                    </v-col>
-                    <v-col>
-                      <v-card-text class="font-weight-bold display-1"
-                        >おもさ:
-                      </v-card-text>
-                    </v-col>
-                    <v-col>
-                      <v-card-text class="font-weight-bold display-1"
-                        >{{ pokeInfo.weight }}kg</v-card-text
-                      >
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col>
-                      <v-card-text class="font-weight-bold display-1"
-                        >タイプ：　くさ/どく</v-card-text
-                      >
-                    </v-col>
-                  </v-row>
+                  <poke-info-text
+                    :row="this.rows[0]"
+                    :column="`たねポケモン`"
+                  />
+                  <poke-info-text
+                    :row="this.rows[1]"
+                    :column="`${pokeInfo.height}m`"
+                  />
+                  <poke-info-text
+                    :row="this.rows[2]"
+                    :column="`${pokeInfo.weight}kg`"
+                  />
+                  <poke-info-text
+                  v-if="pokeInfo.types.length == 2"
+                    :row="this.rows[3]"
+                    :column="`${pokeInfo.types[0].name} ${pokeInfo.types[1].name}`"
+                  />
+                  <poke-info-text
+                  v-if="pokeInfo.types.length == 1"
+                    :row="this.rows[3]"
+                    :column="`${pokeInfo.types[0].name}`"
+                  />
+                </v-card>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col>
+                <v-card shaped class="grey lighten-5" elevation="4">
+                  <v-card-text
+                    class="font-weight-bold display-1 text-center mb-n10"
+                    >ステータス</v-card-text
+                  >
+                  <pokechart :pokeStats="pokeInfo.stats"></pokechart>
                 </v-card>
               </v-col>
             </v-row>
@@ -78,14 +76,15 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import PokeInfo, { fetchPokeInfoById, emptyPokeInfo } from "@/models/pokeInfo";
-import PokeBallSpinner from "./PokeBallSpinner";
-
-const husi: string =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/1.png";
+import PokeBallSpinner from "@/components/PokeBallSpinner.vue";
+import Pokechart from "@/components/Pokechart.vue";
+import PokeInfoText from "@/components/PokeInfoText.vue";
 
 @Component({
   components: {
     PokeBallSpinner,
+    Pokechart,
+    PokeInfoText,
   },
 })
 export default class Pokedex extends Vue {
@@ -93,6 +92,7 @@ export default class Pokedex extends Vue {
   private pokeInfo: PokeInfo = emptyPokeInfo;
   private flgFetched = false;
   private messageNotLoaded = "Loading...";
+  private rows: string[] = ["分類", "たかさ", "おもさ", "タイプ"];
 
   async mounted() {
     this.pokeInfo = await fetchPokeInfoById(this.id, "ja-Hrkt").catch(
