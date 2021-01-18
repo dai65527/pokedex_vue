d18852395d7a916767eeab117acf7fc31def4d8f
Taisei Toya <tytaisei0102@gmail.com>
2021-01-18 12:57:48 +0100

    Add: PokeInfoText
    ポケモンの情報を一行だけ描画するコンポーネントを作成

---
+++ ex02/pokedex/src/components/PokeInfoText.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row>
+    <v-spacer />
+    <v-col cols="5">
+      <v-card-text class="font-weight-bold display-1">{{ this.row }}:</v-card-text>
+    </v-col>
+    <v-col cols="5">
+      <v-card-text class="font-weight-bold display-1">{{ this.column }}</v-card-text>
+    </v-col>
+    <v-spacer />
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class PokeInfoText extends Vue {
+  @Prop()
+  private row!: string;
+  @Prop()
+  private column!: string;
+}
+</script>
