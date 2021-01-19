8904f74a7aaaa42885a97e40299490ed51bef71c
Taisei Toya <tytaisei0102@gmail.com>
2021-01-19 12:04:01 +0100

    Update: loading message
    PokeBallSpinnerコンポーネントの内部にLoading...を表示するよう修正

--- ex02/pokedex/src/components/PokeBallSpinner.vue
+++ ex02/pokedex/src/components/PokeBallSpinner.vue
@@ -1,10 +1,25 @@
 <template>
-    <v-img
-      :src="require('@/assets/pokeball_bg.png')"
-      :class="$style.pokeBall"
-      class="mx-auto my-16"
-      width="150"
-      ></v-img>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-img
+            :src="require('@/assets/pokeball_bg.png')"
+            :class="$style.pokeBall"
+            class="mx-auto my-16"
+            width="150"
+          ></v-img>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" >
+          <p class="mx-auto grey--text text--lighten-1 display-1 text text-center">
+          Loading...
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
