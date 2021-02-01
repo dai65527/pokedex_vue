138f680634aa4d010c69b7d7feed440498ddd2b7
Taisei Toya <tytaisei0102@gmail.com>
2021-02-01 11:46:49 +0100

    Update: next,prev button
    PokemonコンポーネントとpokeInfoCardがページ読み込みなしで違うポケモンを表示できるよう変更

--- ex02/pokedex/src/components/Pokemon.vue
+++ ex02/pokedex/src/components/Pokemon.vue
@@ -13,7 +13,16 @@
             <v-col cols="12" sm="6" class="my-auto">
               <v-row justify="center" align-content="center" no-gutters>
                 <v-col cols="10">
-                  <v-img :src="pokeInfo.imageUrl" :alt="pokeInfo.name" />
+                  <v-img
+                    v-if="!isRefresh"
+                    :src="pokeInfo.imageUrl"
+                    :alt="pokeInfo.name"
+                  />
+                  <poke-ball-spinner
+                    v-if="isRefresh"
+                    :infoText="messageNotLoaded"
+                    :isLoading="flgLoading"
+                  ></poke-ball-spinner>
                 </v-col>
               </v-row>
             </v-col>
@@ -24,12 +33,26 @@
                     shaped
                     class="grey lighten-5"
                     elevation="4"
-                    max-height="100px"
                   >
-                    <v-card-text
-                      class="font-weight-bold text-center display-1"
-                      >{{ `${pokeInfo.name}` }}</v-card-text
-                    >
+                    <v-row class="mt-5 mb-2">
+                      <v-col cols="2" :style="{ 'text-align': 'center' }">
+                        <v-icon v-if="this.id !== 1" @click="refresh(-1)" large
+                          >mdi-menu-left</v-icon
+                        >
+                      </v-col>
+                      <v-col cols="8">
+                        <div
+                          class="font-weight-bold display-1 text-center grey--text text--darken-2"
+                        >
+                          {{ `${pokeInfo.name}` }}
+                        </div>
+                      </v-col>
+                      <v-col cols="2" :style="{ 'text-align': 'center' }">
+                        <v-icon v-if="isPokeMax" @click="refresh(1)" large
+                          >mdi-menu-right</v-icon
+                        >
+                      </v-col>
+                    </v-row>
                   </v-card>
                 </v-col>
               </v-row>
@@ -55,6 +78,8 @@ import PokeBallSpinner from "@/components/PokeBallSpinner.vue";
 import PokeChart from "@/components/PokeChart.vue";
 import PokeInfoCard from "@/components/PokeInfoCard.vue";
 import { Language } from "@/models/language";
+import Pokemon, { POKE_MAX } from "@/models/pokemon";
+import router from "@/router";
 
 @Component({
   components: {
@@ -69,9 +94,14 @@ export default class Pokedex extends Vue {
   private flgFetched = false;
   private flgLoading = true;
   private messageNotLoaded = "Loading...";
+  private isRefresh = false;
 
   get language(): Language {
     return this.$store.state.language;
+  }
+
+  get isPokeMax(): boolean {
+    return this.id !== POKE_MAX;
   }
 
   async fetch() {
@@ -84,12 +114,22 @@ export default class Pokedex extends Vue {
     );
     if (this.pokeInfo !== emptyPokeInfo) {
       this.flgFetched = true;
+      this.isRefresh = false;
+    }
+  }
+
+  async refresh(num: number) {
+    if (!this.isRefresh) {
+      this.isRefresh = true;
+      this.id += num;
+      await this.fetch();
+      router.push({ name: "pokemon", params: { id: String(this.id) } });
     }
   }
 
   @Watch("language")
   async reset() {
-    this.flgFetched = false;
+    this.isRefresh = true;
     await this.fetch();
   }
 
