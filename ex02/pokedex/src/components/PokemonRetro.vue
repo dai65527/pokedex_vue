b2cbe6883498eb5df7ef226e548c87afe824c497
Taisei Toya <tytaisei0102@gmail.com>
2021-02-02 16:08:42 +0100

    Update: reduce gameboy size
    retroコンポーネント全体を0.8倍
    レイアウトを再度調整

--- ex02/pokedex/src/components/PokemonRetro.vue
+++ ex02/pokedex/src/components/PokemonRetro.vue
@@ -1,81 +1,90 @@
 <template>
   <v-container>
-    <v-card
-      class="mx-auto"
-      width="500px"
-      height="500px"
-      img="/retroBg_white.png"
-    >
-      <v-row no-gutters>
-        <v-col class="ma-4">
-          <v-row no-gutters>
-            <v-col class="mt-2"></v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="6" class="mt-11">
-              <v-img
-                :src="pokeRetro.imageUrl"
-                :alt="pokeRetro.name"
-                height="150"
-                contain
-              />
-              <div
-                class="text-center mt-3"
-                :class="$style.infoText"
-                :style="{ 'font-size': '35px', 'letter-spacing': '2px' }"
-              >
-                No.{{ ("000" + this.pokeRetro.id).slice(-3) }}
-              </div>
-            </v-col>
-            <v-col cols="6" class="mt-3">
-              <v-row>
-                <v-col class="my-2">
-                  <span :class="$style.infoText">{{ pokeRetro.name }}</span>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col class="my-2">
-                  <span :class="$style.infoText">{{ pokeRetro.genus }}</span>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col class="my-1">
-                  <span :class="$style.infoText">たかさ </span>
-                  <span
-                    :class="$style.infoText"
-                    class="ml-5"
-                    :style="{ 'font-size': '30px' }"
-                    >{{ pokeRetro.height }}m</span
-                  >
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col class="mb-2">
-                  <span :class="$style.infoText">おもさ </span>
-                  <span
-                    :class="$style.infoText"
-                    class="ml-5"
-                    :style="{ 'font-size': '30px' }"
-                    >{{ pokeRetro.weight }}kg</span
-                  >
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-          <v-row no-gutters>
-            <v-col class="ma-2 pt-3">
-              <div
-                v-for="item in pokeRetro.flavorText"
-                :key="item"
-                :class="$style.infoText"
-                class="my-4"
-              >
-                {{ item }}<br />
-              </div>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
+    <v-card class="mx-auto" width="560px" height="880px" img="/gameboy_bg.png">
+      <v-card
+        class="mx-auto"
+        width="400px"
+        height="400px"
+        img="/retroBg_white.png"
+        :style="{ 'top': '104px', 'overflow': 'hidden' }"
+      >
+        <v-row no-gutters>
+          <v-col class="ma-4">
+            <v-row no-gutters>
+              <v-col class="mt-2"></v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="6">
+                <v-img
+                  :src="pokeRetro.imageUrl"
+                  :alt="pokeRetro.name"
+                  height="150"
+                  contain
+                />
+                <div
+                  class="text-center mt-1"
+                  :class="$style.infoText"
+                  :style="{ 'font-size': '28px', 'letter-spacing': '2px' }"
+                >
+                  No.{{ ("000" + this.pokeRetro.id).slice(-3) }}
+                </div>
+              </v-col>
+              <v-col cols="6" class="mt-1">
+                <v-row>
+                  <v-col class="mt-1">
+                    <span :class="$style.infoText">{{ pokeRetro.name }}</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="mt-1">
+                    <span :class="$style.infoText">{{ pokeRetro.genus }}</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="mt-1">
+                    <span :class="$style.infoText">たかさ </span>
+                    <span
+                      :class="$style.infoText"
+                      class="ml-5"
+                      :style="{ 'font-size': '24px' }"
+                      >{{ pokeRetro.height }}m</span
+                    >
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="mt-1">
+                    <span :class="$style.infoText">おもさ </span>
+                    <span
+                      :class="$style.infoText"
+                      class="ml-5"
+                      :style="{ 'font-size': '24px' }"
+                      >{{ pokeRetro.weight }}kg</span
+                    >
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+            <v-row no-gutters>
+              <v-col class="ma-1 pt-3">
+                <div
+                  v-for="item in pokeRetro.flavorText"
+                  :key="item"
+                  :class="$style.infoText"
+                  class="my-4"
+                >
+                  {{ item }}<br />
+                </div>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-card>
+      <v-img
+        src="/gameboy_btn.png"
+        width="50px"
+        height="50px"
+        :style="{ top: '272px', left: '' }"
+      ></v-img>
     </v-card>
   </v-container>
 </template>
@@ -122,6 +131,6 @@ export default class PokemonRetro extends Vue {
 <style module>
 .infoText {
   font-family: "M PLUS dot" !important;
-  font-size: 25px;
+  font-size: 20px;
 }
 </style>
